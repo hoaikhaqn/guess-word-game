dfc0cdf62732a908e438ffa6624a32492620baaa
hoaikhaqn <hoaikhaqn1996@gmail.com>
2024-05-08 13:18:31 +0700

    messenger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col justify-center container items-center gap-10">
       <div className="mb-10 text-center">
-        <h1 className="text-5xl mb-5 font-bold text-foreground uppercase">Guess word</h1>
+        <h1 className="text-5xl mb-5 font-bold text-foreground uppercase">Guess words</h1>
         <span className="text-4xl px-3 py-1 font-bold text-white uppercase bg-foreground rounded-xl">The game</span>
       </div>
       <MainMenu/>
