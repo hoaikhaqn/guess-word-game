3ff9405cc2e55df4d190bdd89a1de598a9e8b4ce
hoaikhaqn <hoaikhaqn1996@gmail.com>
2024-05-08 13:36:53 +0700

    remove messenger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,29 +11,6 @@ export default function Home() {
       </div>
       <MainMenu />
       {/* <MessegerFB /> */}
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script strategy="lazyOnload" id="facebook-chat">
-        {`
-        var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "102712775611937");
-        chatbox.setAttribute("attribution", "biz_inbox");
-  
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v12.0'
-          });
-        };
-  
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        `}
-      </Script>
     </main>
   )
 }
